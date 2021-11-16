0a872dd7721f9c721545c9e51b4767e5576564f9
quest <edwinjaisonc@gmail.com>
2021-11-16 19:28:18 +0530

    Camera_01 UI design

---
+++ app/src/main/java/com/example/project_camera_01/DataModel.java
@@ -0,0 +1,13 @@
+package com.example.project_camera_01;
+
+public class DataModel {
+
+    public String name;
+    boolean checked;
+
+    DataModel(String name, boolean checked) {
+        this.name = name;
+        this.checked = checked;
+
+    }
+}
